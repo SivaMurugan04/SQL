9718226262b5b771bb6b69ad1765a45dedeb8545
Siva Murugan M <123muthu45@gmail.com>
2024-09-28 22:45:17 +0530

    Add files via upload

---
+++ SQL(Create,Delete,Show).sql
@@ -0,0 +1,10 @@
+CREATE DATABASE databaseStudent;
+
+CREATE DATABASE IF NOT EXISTS databaseStudent;
+
+DROP DATABASE databasestudent;
+
+DROP DATABASE IF  EXISTS databasestudent;
+CREATE DATABASE databaseStudent;
+USE databaseStudent;
+SHOW DATABASES
