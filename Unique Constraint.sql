2a4a10f025f99b192b77404fe7ca9b54c7ecce46
Siva Murugan M <123muthu45@gmail.com>
2024-09-30 22:26:13 +0530

    Add files via upload
    Unique constraint: Ensures values in a column are unique across the table.
    CREATE TABLE example1 (
    phoneNbr INT UNIQUE

---
+++ Unique Constraint.sql
@@ -0,0 +1,15 @@
+DROP DATABASE Student;
+
+CREATE DATABASE College;
+USE College;
+DROP DATABASE College;
+CREATE DATABASE College;
+USE College;
+CREATE TABLE Student(
+age INT UNIQUE);
+INSERT INTO Student
+(age)
+VALUES
+(123),
+(34),
+(872);
